6ce53a57f6e1b23d1752e55408c63de48acc3472
Pratik Chowdhury <pratikc@live.co.uk>
2020-08-27 14:41:20 +0530

    Graph exported as default

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,4 @@
-import { DriverIdAndNameLink } from 'formula1-extract-driver-pairs';
 import Graph from './Graph';
 
-export function GetNumberOfDrivers() {
-   return Object.keys(DriverIdAndNameLink).length;
-}
 export { Graph };
+export default Graph;
